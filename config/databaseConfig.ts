e36db4b726aad16b6b6162995563dde838cc7d97
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 11:18:22 +0200

    Update databaseConfig.ts

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -1,9 +1,11 @@
 module.exports = {
+  connectionString: process.env.DATABASE_URL, // Bruk Heroku DATABASE_URL direkte
   host: process.env.DATABASE_HOST || 'localhost',
   port: process.env.DATABASE_PORT || 5432,
   database: process.env.DATABASE_NAME || 'inovix',
-  user: process.env.DATABASE_USER ||'rahmat',
-  password: process.env.DATABASE_PASSWORD||'',
+  user: process.env.DATABASE_USER || 'rahmat',
+  password: process.env.DATABASE_PASSWORD || '',
   schema: 'public',
   migrationsTable: 'migrations',
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 };
