268400418467f7a57faabbb9e96c60a300662712
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 14:12:33 +0200

    Update databaseConfig.ts

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -1,5 +1,5 @@
 export default {
-   connectionString: process.env.DATABASE_URL + "?sslmode=require", // for Heroku-støtte
+   connectionString: process.env.DATABASE_URL, // for Heroku-støtte
   host: process.env.DATABASE_HOST || 'localhost',
   port: parseInt(process.env.DATABASE_PORT || '5432'),
   database: process.env.DATABASE_NAME || 'inovix',
