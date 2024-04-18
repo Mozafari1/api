93f9771f11a191ac8142236722493e8c5e29f540
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 11:40:17 +0200

    Update databaseConfig.ts

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -1,12 +1,11 @@
-require('dotenv').config(); 
-module.exports = {
-  connectionString: process.env.DATABASE_URL, // Bruk Heroku DATABASE_URL direkte
+export default {
+  connectionString: process.env.DATABASE_URL, // for Heroku-støtte
   host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || 5432,
+  port: parseInt(process.env.DATABASE_PORT || '5432'),
   database: process.env.DATABASE_NAME || 'inovix',
   user: process.env.DATABASE_USER || 'rahmat',
   password: process.env.DATABASE_PASSWORD || '',
   schema: 'public',
   migrationsTable: 'migrations',
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : null
 };
