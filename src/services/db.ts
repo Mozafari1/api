0b2d5d4ab73f653d743ab591ca90b1881b0ecf61
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 14:33:50 +0200

    Update db.ts

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,14 +1,9 @@
 import { Pool } from 'pg';
-import dbConfig from '../../config/databaseConfig'; // Importer konfigurasjonen
-
 const pool = new Pool({
-  connectionString: dbConfig.connectionString, // Prioriter denne for Heroku
-  user: dbConfig.user,
-  host: dbConfig.host,
-  database: dbConfig.database,
-  password: dbConfig.password,
-  port: dbConfig.port,
-  ssl: dbConfig.ssl
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false  // Allows connection to a PostgreSQL instance with a self-signed certificate
+    }
 });
 
 export default pool;
