d5e7e4aadb194986d5f9d7b5072a569794c0adca
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 14:36:54 +0200

    Update migrate-pg-config.js

--- migrate-pg-config.js
+++ migrate-pg-config.js
@@ -1,13 +1,8 @@
 require('dotenv').config();  // Sørger for at .env filen lastes i ikke-produksjonsmiljøer
 
 module.exports = {
-   connectionString: process.env.DATABASE_URL,  // Bruker Heroku DATABASE_URL hvis tilgjengelig
-  user: process.env.DATABASE_USER || 'rahmat', 
-  host: process.env.DATABASE_HOST || 'localhost',
-  database: process.env.DATABASE_NAME || 'inovix',
-  password: process.env.DATABASE_PASSWORD || '',
-  port: process.env.DATABASE_PORT || 5432,
+   connectionString: DATABASE_URL,  // Bruker Heroku DATABASE_URL hvis tilgjengelig
   dir: './migrations',  // Stien til migrasjonsfilene
- ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined
+ ssl: { rejectUnauthorized: false } 
 
 };
