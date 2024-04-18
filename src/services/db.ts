3532a99d812569344665116576f6cbcdbde77950
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2024-04-18 11:24:35 +0200

    Update db.ts

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,12 +1,14 @@
 import { Pool } from 'pg';
+import dbConfig from '../../config/databaseConfig'; // Importer konfigurasjonen
 
-// Konfigurasjon for PostgreSQL-tilkoblingen
 const pool = new Pool({
- user: process.env.DATABASE_USER|| 'rahmat', // Brukernavn for PostgreSQL-databasen
-    host: process.env.DATABASE_HOST||'localhost', // Vert for PostgreSQL-serveren
-    database: process.env.DATABASE_NAME||'inovix', // Navnet på den PostgreSQL-databasen du ønsker å koble til
-    password: process.env.DATABASE_PASSWORD||'', // Passord for PostgreSQL-brukeren
-    port: parseInt(process.env.DATABASE_PORT||'5432'), // Porten som PostgreSQL-serveren kjører på
+  connectionString: dbConfig.connectionString, // Prioriter denne for Heroku
+  user: dbConfig.user,
+  host: dbConfig.host,
+  database: dbConfig.database,
+  password: dbConfig.password,
+  port: dbConfig.port,
+  ssl: dbConfig.ssl
 });
 
 export default pool;
