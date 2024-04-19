020fae7b56510af23e6b38053630e720161cdcb4
R. Mozafari <rahmat@Rs-MacBook-Pro.home>
2024-04-19 09:31:23 +0200

    gitignore + updates

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -6,7 +6,7 @@ const databaseConfig = {
   connectionString: isProduction ? process.env.DATABASE_URL+"?sslmode=require" : process.env.DEVELOPMENT_DATABASE_URL,
   schema: 'public',
   migrationsTable: 'migrations',
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  ssl: isProduction ? { rejectUnauthorized: false,require: true, } : false
 };
 
 export default databaseConfig;
--- migrate-pg-config.js
+++ migrate-pg-config.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     connectionString: isProduction ? process.env.DATABASE_URL+"?sslmode=require" : process.env.DEVELOPMENT_DATABASE_URL,
-  dir: './migrations',  // Stien til migrasjonsfilene
- ssl: { rejectUnauthorized: false } 
+    dir: './migrations', 
+    ssl: { rejectUnauthorized: false, require: true, } 
 
 };
