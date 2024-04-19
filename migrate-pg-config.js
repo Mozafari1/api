ae5d8bca73e6ef38a5257d9eb8de0f7dc7822503
R. Mozafari <rahmat@Rs-MacBook-Pro.home>
2024-04-19 09:59:09 +0200

    gitignore + updates

--- migrate-pg-config.js
+++ migrate-pg-config.js
@@ -5,8 +5,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
     connectionString: isProduction ? process.env.DATABASE_URL: process.env.DEVELOPMENT_DATABASE_URL,
   dir: './migrations',  // Stien til migrasjonsfilene
-  ssl:isProduction? {
+  ssl:{
     sslmode: true ,
-    rejectUnauthorized: false   } : false
+    rejectUnauthorized: false   }
 
 };
