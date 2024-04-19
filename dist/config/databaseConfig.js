a5acfda6a3946d89c902bdaeea9d0b6de51d88ee
R. Mozafari <rahmat@Rs-MacBook-Pro.home>
2024-04-19 15:26:03 +0200

    gitignore + updates

--- dist/config/databaseConfig.js
+++ dist/config/databaseConfig.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 const databaseConfig = {
-    connectionString: `${isProduction ? process.env.DATABASE_URL + "?sslmode=require" : process.env.DEVELOPMENT_DATABASE_URL}`,
+    connectionString: isProduction
+        ? `${process.env.DATABASE_URL}?sslmode=require`
+        : process.env.DEVELOPMENT_DATABASE_URL,
+    ssl: isProduction ? { rejectUnauthorized: false } : undefined,
     schema: 'public',
     migrationsTable: 'migrations',
-    ssl: isProduction ? {
-        rejectUnauthorized: false,
-    } : false
 };
 exports.default = databaseConfig;
