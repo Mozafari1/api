96076362e307c96acdec35752379492c356a32e6
R. Mozafari <rahmat@Rs-MacBook-Pro.home>
2024-04-19 10:08:32 +0200

    gitignore + updates

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -5,10 +5,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 const databaseConfig = {
   connectionString: isProduction ? process.env.DATABASE_URL : process.env.DEVELOPMENT_DATABASE_URL,
   schema: 'public',
-  migrationsTable: 'migrations',
+    migrationsTable: 'migrations',
+  
     ssl: isProduction ? {
+        sslmode: 'require',
         rejectUnauthorized: false,
-        sslmode: true
+   
   } : false
 };
 
--- migrate-pg-config.js
+++ migrate-pg-config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    connectionString: isProduction ? process.env.DATABASE_URL: process.env.DEVELOPMENT_DATABASE_URL,
-  dir: './migrations',  // Stien til migrasjonsfilene
-  ssl:{
-    sslmode: true ,
-    rejectUnauthorized: false   }
-
+    connectionString: isProduction ? process.env.DATABASE_URL : process.env.DEVELOPMENT_DATABASE_URL,
+    dir: './migrations', // Stien til migrasjonsfilene
+    ssl: isProduction ? {
+        sslmode: 'require', // Bekrefter at SSL er påkrevd
+        rejectUnauthorized: false // Tillater selvsignerte sertifikater, viktig på Heroku
+    } : false
 };
